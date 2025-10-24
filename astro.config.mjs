7f52cd5366bcaaf35415892d6fafbc852f13d4f1
churchilldotexe <churchillexe@gmail.com>
2025-10-25 00:55:53 +0800

    chore: remove icons and use astro integrated icon instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site:"https://leivaassistants.netlify.app/",
@@ -12,5 +14,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [sitemap()]
+  integrations: [sitemap(), icon()]
 });
